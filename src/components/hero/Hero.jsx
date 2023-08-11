e977d318a0e94bd687dad336d438345bcbd46ee0
gabr3924 <garc2004@gmail.com>
2023-08-11 17:43:47 -0400

    first commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,17 +3,27 @@ import css from './Hero.module.css'
 import Nav from '../nav/Nav'
 import { BsArrowDownCircle } from 'react-icons/bs'
 import logo from '../../assets/launch.png'
-
+import video from "../../assets/portVideo.mp4"
+import ReactPlayer from 'react-player';
 
 
 const Hero = () => {
   return (
     <div className={css.section}>
+       <ReactPlayer
+        url =  {video}
+        playing
+        loop
+        muted
+        width="100%"
+        height="100%"
+       
+      />
       <Nav/>
       <div className={css.container}>
-        <div className={css.left}>
+        <div className={css.left}>  
           <div className={css.text}>
-          <h1>Hi Im a web developer</h1>
+          <h1>Hola</h1>
           <span>We enjoy creating delightful, human-centered digital experiences.</span>
           </div>
           <div className={css.btns}>
@@ -23,7 +33,7 @@ const Hero = () => {
         </div>
         <div className={css.right}>
 
-          <img className={css.logo} src={logo} alt="" />
+          {/* <img className={css.logo} src={logo} alt="" /> */}
 
         </div>
       </div>
